be9783413e88b0efab9fd13865dfb8715812282c
aditya7iyengar <adi.iyengar@annkissam.com>
2016-08-12 10:21:57 -0400

    Add test encryption

--- encrypt_test.rb
+++ encrypt_test.rb
@@ -1,4 +1,4 @@
-# C = (P^2+1)mod(P.length)
+# C = (P+2)mod(P+1)
 
 def char_to_int(character)
   alphabets_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -31,7 +31,7 @@ def int_to_char(integer)
 end
 
 def inner_function(plain_text_integer, modulo_base)
-  (plain_text_integer**2 + 1) % modulo_base
+  (plain_text_integer + 2) % modulo_base
 end
 
 def encrypt_function(plain_text)
@@ -40,7 +40,7 @@ def encrypt_function(plain_text)
 
   plain_text.split("").each do |plain_text_character|
     plain_text_integer = char_to_int(plain_text_character)
-    cipher_text_integer = inner_function(plain_text_integer, plain_text_length)
+    cipher_text_integer = inner_function(plain_text_integer, plain_text_integer + 1)
     cipher_text_character = int_to_char(cipher_text_integer)
 
     cipher_text << cipher_text_character
